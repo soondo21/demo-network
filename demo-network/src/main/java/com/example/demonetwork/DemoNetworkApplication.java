649113045ce451ffc043fc3c3308864a8aad6811
soondo_kwon <soondo_kwon@tmax.co.kr>
2022-03-15 15:12:30 +0900

    first demo-network project commit & push

---
+++ demo-network/src/main/java/com/example/demonetwork/DemoNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demonetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNetworkApplication.class, args);
+	}
+
+}
---
+++ demo-network/src/test/java/com/example/demonetwork/DemoNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demonetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
